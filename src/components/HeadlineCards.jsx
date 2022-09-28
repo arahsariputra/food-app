f70e5e70ec40c1e76cc0e1e0372979ce710f516d
arah sari putra <arahsari@gmail.com>
2022-09-29 04:50:40 +0700

    first React-Food commit

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const HeadlineCards = () => {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+        {/* Card */}
+        <div className='rounded-xl relative'>
+            {/* Overlay */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, Bogo's Out</p>
+                <p className='px-2'>Throught 8/26</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img 
+            className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+            src='https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg' alt='/' />
+        </div>
+        {/* Card */}
+        <div className='rounded-xl relative'>
+            {/* Overlay */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>New Restaurant</p>
+                <p className='px-2'>Added Daily</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img 
+            className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+            src='https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='/' />
+        </div>
+        {/* Card */}
+        <div className='rounded-xl relative'>
+            {/* Overlay */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>We Deliver Desserts Too</p>
+                <p className='px-2'>Tasty Treats</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img 
+            className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+            src='https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='/' />
+        </div>
+    </div>
+  )
+}
+
+export default HeadlineCards
